8b4e442a7f028030e9bcfd1a05196c35ca104d37
Quoc Phap <QuocPhap@Phap-Mac-M1.local>
2023-02-09 16:28:56 +0700

    daochieuxeoto

---
+++ khaibaovasudungbien(jss)/index.js
@@ -0,0 +1,12 @@
+var i =10;
+var f =20.5;
+var b =true;
+var s="Hà Nôi";
+
+document.write("i = 10"  + s);
+document.write("<br/>");
+document.write(s +f);
+document.write("<br/>");
+document.write("b="+b);
+document.write("<br/>");
+document.write("s="+s);
