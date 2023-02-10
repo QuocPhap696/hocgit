ce0b6e8652c5edf7259a7e913683ab410ef93379
Quoc Phap <QuocPhap@Phap-Mac-M1.local>
2023-02-10 15:18:41 +0700

    dienthichhinhchunhat

---
+++ toantu/java.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("nhap vao Wight")
+inputHeight = prompt("nhap vao height")
+
+let wight = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = wight * height;
+document.write('The area ' + area)
