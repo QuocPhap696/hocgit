27356bda1b9a64f3d6d21b5fd29faedc930f9737
Quoc Phap <QuocPhap@Phap-Mac-M1.local>
2023-02-12 21:20:38 +0700

    hienthiketqua

---
+++ toantu/java.js
@@ -0,0 +1,34 @@
+function Addition() {
+    let so1 =document.getElementById("so11").value;
+    let so2 = document.getElementById("so22").value;
+
+    let ketqua = ('so1 + so2');
+
+  document.getElementById("cong");
+}
+function Subtraction() {
+    let so1 =document.getElementById("so1").value;
+    let so2 = document.getElementById("so2").value;
+    document.getElementById("tru");
+
+    let ketqua = ('so1 - s02');
+
+   alert('ket qua hien thi');
+}
+function Multiplication() {
+    let so1 =document.getElementById("so1").value;
+    let so2 = document.getElementById("so2").value;
+
+    let ketqua = ('so1 * s02');
+
+   alert('ket qua hien thi');
+}
+function Division() {
+    let so1 =document.getElementById("so1").value;
+    let so2 = document.getElementById("so2").value;
+    document.getElementById("chia");
+
+    let ketqua = ('so1 / s02');
+
+   alert('ket qua hien thi');
+}
