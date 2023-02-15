1fd8e9141e4f883143cea5f5eb1842e572d7c48c
Quoc Phap <QuocPhap@Phap-Mac-M1.local>
2023-02-15 16:00:22 +0700

    gamepuzzle

---
+++ Gamepuzzle/java.js
@@ -0,0 +1,13 @@
+function myFunction() {
+        document.getElementById("so1").src="img/cap1.jpg"
+       }
+function myFunction1() {
+
+        document.getElementById("hihi").src="img/cap2.jpg"
+       }
+function myFunction5() {
+
+        document.getElementById("so3").src="img/cap3..jpg"
+       }
+
+
