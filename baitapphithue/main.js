0f67a1ae36f560dbc60f2836ad91e8086d9995b8
Quoc Phap <QuocPhap@Phap-Mac-M1.local>
2023-02-16 16:08:29 +0700

    cau truc dieu kien

---
+++ baitapphithue/main.js
@@ -0,0 +1,25 @@
+
+let thunhap = 100;
+switch (thue) 
+{case (a >= 5):
+console.log ('Mức thuế thu nhập là 5%'); 
+ break;  
+    case( 5 > a >= 10):
+    console.log('Mức thuế thu nhập của bạn là 10%')
+    break;
+        case( 10 > a >= 18):
+        console.log('Mức thuế thu nhập của bạn là 15%');
+        break;
+            case( 18 > a >= 32):
+            console.log('Mức thuế thu nhập của bạn là 20%');
+            break;
+                case( 32 > a >= 52):
+                console.log('Mức thuế thu nhập của bạn là 25%');
+                break;
+                    case( 52 > a >= 80):
+                    console.log('Mức thuế thu nhập của bạn là 25%');
+                    break;
+                        case( a >= 80):
+                        console.log('Mức thuế thu nhập của bạn là 30%');
+                        break;          
+}
